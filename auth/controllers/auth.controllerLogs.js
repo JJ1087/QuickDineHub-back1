2f69db47ae0ba59a0a607ee9034b36adeeb3d2c8
GUILLEJULIO <20200775@uthh.edu.mx>
2024-04-19 07:27:16 -0600

    CORREOS Y LOGS

--- auth/controllers/auth.controllerLogs.js
+++ auth/controllers/auth.controllerLogs.js
@@ -2,76 +2,134 @@
 const AutenticacionLog = require('../models/logs-Autentificacion');
 const ErrorLog = require('../models/logs-Error');
 
+const User = require('../models/auth.model');
+const logacceso = require('../models/logs-Acceso'); //log acceso
 
 
 
 // Controlador para agregar log de autenticación---------------------------------------------------------------
-exports.agregarLogAutenticacion = async (req, res) => {
-  try {
-    // Recibe los datos del cuerpo de la solicitud
-    const { userCorreo, tipoEdicion } = req.body;
-    console.log('email:', userCorreo, 'edicion:', tipoEdicion);
+exports.agregarLogAutenticacion = async(req, res) => {
+    try {
+        // Recibe los datos del cuerpo de la solicitud
+        const { userCorreo, tipoEdicion } = req.body;
+        console.log('email:', userCorreo, 'edicion:', tipoEdicion);
 
-    // Crea un nuevo registro de log de autenticación
-    const nuevoLog = new AutenticacionLog({
-      userCorreo,
-      tipoEdicion
-    });
+        // Crea un nuevo registro de log de autenticación
+        const nuevoLog = new AutenticacionLog({
+            userCorreo,
+            tipoEdicion
+        });
 
-    // Guarda el registro en la base de datos
-    await nuevoLog.save();
+        // Guarda el registro en la base de datos
+        await nuevoLog.save();
 
-    // Envía una respuesta de éxito
-    res.status(200).json({ message: 'Registro de log de autenticación agregado con éxito' });
-  } catch (error) {
-    // Si hay un error, envía una respuesta de error
-    console.error('Error al agregar el registro de log de autenticación:', error);
-    res.status(500).json({ message: 'Error al agregar el registro de log de autenticación' });
-  }
+        // Envía una respuesta de éxito
+        res.status(200).json({ message: 'Registro de log de autenticación agregado con éxito' });
+    } catch (error) {
+        // Si hay un error, envía una respuesta de error
+        console.error('Error al agregar el registro de log de autenticación:', error);
+        res.status(500).json({ message: 'Error al agregar el registro de log de autenticación' });
+    }
 };
 
 let contadorErrores = 0;
 let mensaje = "";
-exports.registrarError = async (req, res) => {
-  try {
-    // Recibe los datos del cuerpo de la solicitud
-    const { errorDetails, errorType } = req.body;
-    console.log('Detalles del error:', errorDetails, 'Tipo de error:', errorType);
+exports.registrarError = async(req, res) => {
+    try {
+        // Recibe los datos del cuerpo de la solicitud
+        const { errorDetails, errorType } = req.body;
+        console.log('Detalles del error:', errorDetails, 'Tipo de error:', errorType);
 
 
-    // Busca en la base de datos los dos errores más recientes con los mismos detalles
-    const ultimosErrores = await ErrorLog.find({ errorDetails }).sort({ timestamp: -1 }).limit(3);
+        // Busca en la base de datos los dos errores más recientes con los mismos detalles
+        const ultimosErrores = await ErrorLog.find({ errorDetails }).sort({ timestamp: -1 }).limit(3);
 
-      if (ultimosErrores.length === 3) {
+        if (ultimosErrores.length === 3) {
 
-        contadorErrores++;
-        mensaje ="";
-        console.log("CONTADOR FUERA DE IF", contadorErrores); 
-        if (contadorErrores === 3) {
+            contadorErrores++;
+            mensaje = "";
+            console.log("CONTADOR FUERA DE IF", contadorErrores);
+            if (contadorErrores === 3) {
 
-        mensaje ="Advertencia"
-        contadorErrores = 0; // Reinicia el contador después de enviar la advertencia
-        console.log("Entramos AL IF MAS PROFUNDO DEBEMOS REINICIAR", contadorErrores);
-        //return;
-      }
+                mensaje = "Advertencia"
+                contadorErrores = 0; // Reinicia el contador después de enviar la advertencia
+                console.log("Entramos AL IF MAS PROFUNDO DEBEMOS REINICIAR", contadorErrores);
+                //return;
+            }
+        }
+
+        // Crea un nuevo registro de log de error
+        const nuevoErrorLog = new ErrorLog({
+            errorDetails,
+            errorType
+        });
+
+        // Guarda el registro en la base de datos
+        await nuevoErrorLog.save();
+
+        // Envía una respuesta de éxito
+        console.log("mensaje antes", mensaje);
+        res.status(200).json({ message: 'Registro de log de error agregado con éxito', mensaje });
+        console.log("mensajedespues", mensaje);
+    } catch (error) {
+        // Si hay un error, envía una respuesta de error
+        console.error('Error al agregar el registro de log de error:', error);
+        res.status(500).json({ message: 'Error al agregar el registro de log de error' });
     }
+};
 
-    // Crea un nuevo registro de log de error
-    const nuevoErrorLog = new ErrorLog({
-      errorDetails,
-      errorType
-    });
 
-    // Guarda el registro en la base de datos
-    await nuevoErrorLog.save();
 
-    // Envía una respuesta de éxito
-    console.log("mensaje antes", mensaje); 
-    res.status(200).json({ message: 'Registro de log de error agregado con éxito', mensaje}); 
-    console.log("mensajedespues", mensaje); 
-  } catch (error) {
-    // Si hay un error, envía una respuesta de error
-    console.error('Error al agregar el registro de log de error:', error);
-    res.status(500).json({ message: 'Error al agregar el registro de log de error' });
-  }
+// LOG DE ACCESO Y FUERZA BRUTA---------------------------------------------------------------
+const AccesoLog = require('../models/logs-Acceso'); // Importa el modelo de log de acceso
+
+exports.actualizarIntentosFallidos = async(req, res) => {
+    const { userEmail, intentosFallidos } = req.body;
+    console.log('Editar:', userEmail);
+    console.log('Nuevo contador de intentos fallidos:', intentosFallidos);
+
+    try {
+        // Encuentra el registro de log más reciente para el usuario
+        const latestAccessLog = await AccesoLog.findOne({ email: userEmail }).sort({ createdAt: -1 });
+
+        // Actualiza el contador de intentos fallidos
+        latestAccessLog.intentosFallidos = intentosFallidos;
+
+        // Guarda los cambios en el registro de log
+        await latestAccessLog.save();
+
+        // Envía una respuesta de éxito con el nuevo valor de intentosFallidos
+        res.status(200).json({
+            message: 'Contador de intentos fallidos actualizado correctamente',
+            _id: latestAccessLog._id,
+            intentosFallidos: latestAccessLog.intentosFallidos,
+            inicioSesion: latestAccessLog.inicioSesion,
+            user_id: latestAccessLog.user_id
+        });
+    } catch (error) {
+        // Manejo de errores
+        console.error('Error al actualizar los intentos fallidos:', error);
+        res.status(500).json({ message: 'Error al actualizar los intentos fallidos' });
+    }
+};
+
+exports.obtenerIntentosFallidos = async(req, res) => {
+    const { userEmail } = req.query;
+
+    try {
+        // Encuentra el registro de log más reciente para el usuario
+        const latestAccessLog = await AccesoLog.findOne({ email: userEmail }).sort({ createdAt: -1 });
+
+        // Si no se encuentra el registro o no tiene intentos fallidos, devolver 0
+        if (!latestAccessLog || !latestAccessLog.intentosFallidos) {
+            return res.status(200).json({ intentosFallidos: 0 });
+        }
+
+        // Devolver el número de intentos fallidos
+        res.status(200).json({ intentosFallidos: latestAccessLog.intentosFallidos });
+    } catch (error) {
+        // Manejo de errores
+        console.error('Error al obtener los intentos fallidos:', error);
+        res.status(500).json({ message: 'Error al obtener los intentos fallidos' });
+    }
 };
--- auth/controllers/correo.controller.js
+++ auth/controllers/correo.controller.js
@@ -81,19 +81,19 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-    user: 'quickdinehub@gmail.com', // Cambiar por tu dirección de correo electrónico
-    pass: 'crqpsebcojympajd' // Cambiar por tu contraseña de correo electrónico
-  }
+        user: 'quickdinehub@gmail.com', // Cambiar por tu dirección de correo electrónico
+        pass: 'crqpsebcojympajd' // Cambiar por tu contraseña de correo electrónico
+    }
 });
 
 function enviarCorreo(req, res) {
-  const { email, codigo } = req.body;
+    const { email, codigo } = req.body;
 
-  const mailOptions = {
-    from: 'quickdinehub@gmail.com', // Cambiar por tu dirección de correo electrónico
-    to: email,
-    subject: 'Código de autenticación',
-    html: `
+    const mailOptions = {
+        from: 'quickdinehub@gmail.com', // Cambiar por tu dirección de correo electrónico
+        to: email,
+        subject: 'Código de autenticación',
+        html: `
       <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
         <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
           <div style="text-align: center; padding: 20px;">
@@ -108,22 +108,130 @@ function enviarCorreo(req, res) {
         </div>
       </div>
     `,
-    //text: `Tu código de autenticación es: ${codigo}`
-  };
+        //text: `Tu código de autenticación es: ${codigo}`
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error al enviar el correo electrónico:', error);
-      res.status(500).json({ error: 'Error al enviar el correo electrónico' });
-    } else {
-      console.log('Correo electrónico enviado:', info.response);
-      res.status(200).json({ message: 'Correo electrónico enviado exitosamente' });
-    }
-  });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error al enviar el correo electrónico:', error);
+            res.status(500).json({ error: 'Error al enviar el correo electrónico' });
+        } else {
+            console.log('Correo electrónico enviado:', info.response);
+            res.status(200).json({ message: 'Correo electrónico enviado exitosamente' });
+        }
+    });
 }
+//ADVERTENCIA: MUCHOS INTENTOS FALLIDOS DE INICIO DE SESION
+function enviarCorreoAdvertencia(req, res) {
+    const { email } = req.body;
 
-module.exports = {
-  enviarCorreo
+    const mailOptions = {
+        from: 'quickdinehub@gmail.com', // Cambiar por tu dirección de correo electrónico
+        to: email,
+        subject: 'Advertencia:Intentos de acceso fallidos a su cuenta',
+        html: `
+      <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
+        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
+          <div style="text-align: center; padding: 20px;">
+            <img src="https://qdh-interfazprueba.web.app/assets/logo.png" alt="Austin's Logo" style="max-width: 100px;">
+          </div>
+          <div style="text-align: center; padding: 20px;">
+            <h2 style="font-size: 24px; color: #333;">Advertencia: Intentos de acceso fallidos a su cuenta</h2>
+            <p style="color: #555; font-size: 16px;">Queremos informarle que hemos detectado varios intentos fallidos de acceso a su cuenta en nuestro sistema durante las últimas horas. Como medida de seguridad, le recomendamos encarecidamente que verifique su cuenta y asegúrese de que su contraseña sea segura y no esté comprometida.</p>
+          </div>
+          <p style="text-align: center; color: #777; font-size: 16px;">Si reconoce estos intentos de acceso fallidos como suyos, le sugerimos que revise sus credenciales y evite el acceso no autorizado a su cuenta. Si no reconoce estos intentos de acceso, le instamos a que cambie su contraseña inmediatamente y a que se ponga en contacto con nuestro equipo de soporte para obtener asistencia adicional.</p>
+        </div>
+      </div>
+    `,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error al enviar el correo electrónico:', error);
+            res.status(500).json({ error: 'Error al enviar el correo electrónico' });
+        } else {
+            console.log('Correo electrónico enviado:', info.response);
+            res.status(200).json({ message: 'Correo electrónico enviado exitosamente' });
+        }
+    });
 };
 
+//AL RESTABLECER CONTRASEÑA
+function enviarCorreBloqueo(req, res) {
+    const { email } = req.body;
 
+    const mailOptions = {
+        from: 'quickdinehub@gmail.com', // Cambiar por tu dirección de correo electrónico
+        to: email,
+        subject: 'Asunto: Notificación de Bloqueo de Cuenta',
+        html: `
+  <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
+    <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
+      <div style="text-align: center; padding: 20px;">
+        <img src="https://qdh-interfazprueba.web.app/assets/logo.png" alt="Austin's Logo" style="max-width: 100px;">
+      </div>
+      <div style="text-align: center; padding: 20px;">
+        <h2 style="font-size: 24px; color: #333;">Asunto: Notificación de Bloqueo de Cuenta</h2>
+        <p style="color: #555; font-size: 16px;">Lamentamos informarle que su cuenta ha sido bloqueada temporalmente debido a múltiples intentos fallidos de acceso. Esta medida de seguridad se ha tomado para proteger su información y prevenir accesos no autorizados a su cuenta.</p>
+        </div>
+      <p style="text-align: justify; color: #777; font-size: 16px;">Para desbloquear su cuenta, le recomendamos que siga los siguientes pasos:</p>
+      <p style="text-align: justify; color: #777; font-size: 14px;">1. Cambie su contraseña: Le sugerimos que restablezca su contraseña de inmediato para garantizar la seguridad de su cuenta.</p>
+      <p style="text-align: justify; color: #777; font-size: 14px;">2. Verificación de Seguridad: Si sospecha que su cuenta pudo haber sido comprometida, le recomendamos que realice una verificación de seguridad adicional para asegurarse de que su cuenta esté protegida.</p>
+      <p style="text-align: justify; color: #777; font-size: 14px;">3. Contacte con Soporte: Si necesita ayuda adicional o tiene alguna pregunta, no dude en ponerse en contacto con nuestro equipo de soporte, quienes estarán encantados de ayudarlo a resolver cualquier problema relacionado con su cuenta.</p>
+    
+      </div>
+  </div>
+`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error al enviar el correo electrónico:', error);
+            res.status(500).json({ error: 'Error al enviar el correo electrónico' });
+        } else {
+            console.log('Correo electrónico enviado:', info.response);
+            res.status(200).json({ message: 'Correo electrónico enviado exitosamente' });
+        }
+    });
+}
+
+//AL RESTABLECER CONTRASEÑA
+function enviarCorreRestablecimiento(req, res) {
+    const { email } = req.body;
+
+    const mailOptions = {
+        from: 'quickdinehub@gmail.com', // Cambiar por tu dirección de correo electrónico
+        to: email,
+        subject: 'Asunto: Confirmación de Cambio de Contraseña',
+        html: `
+    <div style="background-color: #f5f5f5; padding: 20px; font-family: 'Arial', sans-serif;">
+      <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);">
+        <div style="text-align: center; padding: 20px;">
+          <img src="https://qdh-interfazprueba.web.app/assets/logo.png" alt="Austin's Logo" style="max-width: 100px;">
+        </div>
+        <div style="text-align: center; padding: 20px;">
+          <h2 style="font-size: 24px; color: #333;">Asunto: Confirmación de Cambio de Contraseña</h2>
+          <p style="color: #555; font-size: 16px;">Le informamos que hemos recibido su solicitud para cambiar la contraseña de su cuenta en QuickDineHub. Se ha realizado con éxito el cambio de contraseña y su cuenta ahora está protegida con una nueva contraseña.</p>
+          </div>
+        <p style="text-align: center; color: #777; font-size: 16px;">Si usted no solicitó este cambio de contraseña o considera que su cuenta podría estar comprometida, le recomendamos que se ponga en contacto con nuestro equipo de soporte de inmediato para revisar la situación y tomar las medidas necesarias para proteger su cuenta.</p>
+      </div>
+    </div>
+  `,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error al enviar el correo electrónico:', error);
+            res.status(500).json({ error: 'Error al enviar el correo electrónico' });
+        } else {
+            console.log('Correo electrónico enviado:', info.response);
+            res.status(200).json({ message: 'Correo electrónico enviado exitosamente' });
+        }
+    });
+}
+module.exports = {
+    enviarCorreo,
+    enviarCorreoAdvertencia,
+    enviarCorreRestablecimiento,
+    enviarCorreBloqueo
+};
