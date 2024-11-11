e65322bd5580ef104d05158f3e29f5f9d371d47f
juliogmo <20200775@uthh.edu.mx>
2024-11-10 20:42:19 -0600

    "notificaciones push en back"

---
+++ auth/models/auth.modelpushsubscription.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const pushSubscriptionSchema = new mongoose.Schema({
+  endpoint: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  expirationTime: {
+    type: Date,
+    default: null,
+  },
+  keys: {
+    auth: {
+      type: String,
+      required: true,
+    },
+    p256dh: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+
+module.exports = mongoose.model('PushSubscription', pushSubscriptionSchema);
